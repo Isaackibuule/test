ac4a42f9655d11562ea3058f87f414edfb310d71
Isaackibuule <isaackibule@gmail.com>
2025-02-18 13:15:17 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+let menu = document.querySelector('#menu');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick=()=>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () =>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+}
 
 
 document.addEventListener("DOMContentLoaded", function() {
@@ -12,118 +24,4 @@ document.addEventListener("DOMContentLoaded", function() {
         acrelev.appendChild(span);
     });
 });
-
-
-
-document.querySelector("#get-started" && "#Register").addEventListener("click", function(event) {
-    // event.preventDefault(); // Prevent the default link behavior
-    document.querySelector(".popup").classList.add("active");
-});
-
-document.querySelector(".popup .close-btn").addEventListener("click", function() {
-    document.querySelector(".popup").classList.remove("active");
-});
-
-/*
-
-document.querySelector("#Register").addEventListener("click", function(event) {
-    event.preventDefault(); // Prevent the default link behavior
-    document.querySelector(".modal-content").classList.add("active");
-});
-
-document.querySelector(".popup .close-btn").addEventListener("click", function() {
-    document.querySelector(".popup").classList.remove("active");
-});
-*/
-
-
-/* Redirecting
-document.getElementById("Register").addEventListener("click", () =>{
-    window.location.href = "registration.html";
-});
-*/
-
-
-
-document.addEventListener("DOMContentLoaded", function() {
-    const learnMoreBtns = document.querySelectorAll('.learn-more-btn');
-    const modal = document.getElementById('learn-more-modal');
-    const closeBtn = document.querySelector('.close-btn');
-
-    learnMoreBtns.forEach(btn => {
-        btn.onclick = () => {
-            modal.style.display = 'block';
-        }
-    });
-
-    closeBtn.onclick = () => {
-        modal.style.display = 'none';
-    }
-
-    window.onclick = (event) => {
-        if (event.target == modal) {
-            modal.style.display = 'none';
-        }
-    }
-
-
-
-
-    // Darkmode theme
-    let darkmode = localStorage.getItem('darkmode')
-    const themeSwitch = document.getElementById('theme-switch')
-    
-    const enableDarkmode = () => {
-        document.body.classList.add('darkmode')
-        localStorage.setItem('darkmode', 'active')
-    
-    //invert filter
-        const mapIframe = document.querySelector('.Mapcontainer iframe');
-    if(mapIframe) {
-        mapIframe.style.filter= 'invert(100%)';
-    
-    }
-    
-        
-    }
-    
-    const disableDarkmode = () => {
-        document.body.classList.remove('darkmode')
-        localStorage.setItem('darkmode', null)
-    
-        //remove invert 
-        const mapIframe = document.querySelector('.Mapcontainer iframe');
-    if(mapIframe) {
-        mapIframe.style.filter= 'none';
-    
-    }
-    
-    }
-    
-    if(darkmode === "active") enableDarkmode()
-    
-    themeSwitch.addEventListener("click", () => {
-        darkmode = localStorage.getItem('darkmode')
-        darkmode !== "active" ? enableDarkmode() : disableDarkmode()
-    })
-    
-    
-    
-    
-        
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
 
